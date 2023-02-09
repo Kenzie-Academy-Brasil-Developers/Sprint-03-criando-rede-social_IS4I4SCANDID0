237a048fdbf6d6d2d95140e1a878067c0d14a97f
Isaias <isaiascandido92@outlook.com>
2023-02-08 22:28:50 -0300

    inicia a renderização com DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,58 +1,60 @@
 // import { suggestUsers, posts } from './database;js'
 
 function renderPosts(arrayPosts) {
-    const ulPosts = document.querySelector('ul')
-    ulPosts.classList.add('container__posts')
+  const ulPosts = document.querySelector('ul')
+  ulPosts.classList.add('container__posts')
 
-    ulPosts.innerHTML = ''
-    
-    arrayPosts.forEach(post => {
-        
-        const publication = createPosts(post)
+  ulPosts.innerHTML = ''
 
-        ulPosts.appendChild(publication)
-    })
+  arrayPosts.forEach(post => {
+
+    const publication = createPosts(post)
+
+    ulPosts.appendChild(publication)
+  })
 }
 
 
 
 function createPosts(post) {
-    const publications = document.createElement('li')
-    publications.classList.add('posts--item')
+  const publications = document.createElement('li')
+  publications.classList.add('posts--item')
 
-    const userBox = document.createElement('div')
-    userBox.classList.add('item--user')
+  const userBox = document.createElement('div')
+  userBox.classList.add('item--user')
 
-    const userImg = document.createElement('img')
-    userImg.src = post.img
+  const userImg = document.createElement('img')
+  userImg.src = post.img
 
-    const userInfo = document.createElement('div')
-    userInfo.classList.add('item--userInfo')
+  const userInfo = document.createElement('div')
+  userInfo.classList.add('item--userInfo')
 
-    const userName = document.createElement('h2')
+  const userName = document.createElement('h2')
 
-    const userStack = document.createElement('p')
+  const userStack = document.createElement('p')
 
-    userInfo.append(userName, userStack)
+  userInfo.append(userName, userStack)
 
-    userBox.append(userImg, userInfo)
+  userBox.append(userImg, userInfo)
 
-    const postTitle = document.createElement('h2')
-    postTitle.innerText = post.title
+  const postTitle = document.createElement('h2')
+  postTitle.innerText = post.title
 
-    const postContent = document.createElement('p')
-    postContent.innerText = `${post.title.substring(0, 500)}`
-    
-    const postInteract = document.createElement('div')
-    postInteract.classList.add('posts--interactions')
+  const postContent = document.createElement('p')
+  postContent.innerText = `${post.title.substring(0, 500)}`
 
-    const openPost = document.createElement('button')
-    // colocar dataset
-    
-    const postLikes = document.createElement('i')
-    postLikes.classList.add('fa-solid , fa-heart')
+  const postInteract = document.createElement('div')
+  postInteract.classList.add('posts--interactions')
 
-    // return publications
-    //revisar tudo com calma e descobrir como acessar
-    //o data base com import
+  const openPost = document.createElement('button')
+  // colocar dataset
+
+  const postLikes = document.createElement('i')
+  postLikes.classList.add('fa-solid , fa-heart')
+
+  postInteract.append(openPost, postLikes)
+
+  // return publications
+  //revisar tudo com calma e descobrir como acessar
+  //o data base com import
 }
